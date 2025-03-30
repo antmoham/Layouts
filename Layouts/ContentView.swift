736d2d435d4c530cbb052bf6faefb5cd645ffa14
Anthony Moham <antmoham@icloud.com>
2025-03-30 15:24:25 -0700

    Initial Commit

---
+++ Layouts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by Anthony on 3/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
